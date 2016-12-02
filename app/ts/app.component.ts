a5a9351b4fdb113ce11f0967bc8ee99d969a7fbc
Vladimir Pavković <vladimirpavk@gmail.com>
2016-12-02 15:42:21 +0100

    Initial commit

---
+++ app/ts/app.component.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../node_modules/@angular/common/index.d.ts" />
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: '/ts/app.component.html'
+})
+export class AppComponent { }
---
+++ app/ts/app.module.ts
@@ -0,0 +1,10 @@
+/// <reference path="../../node_modules/@angular/common/index.d.ts" />
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
