b7cf1986ca8c21154a0f8120591829333bd09536
Korisnik <vladimirpavk@gmail.com>
2016-12-02 23:52:11 +0100

    Finally working

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -2,6 +2,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  templateUrl: '/ts/app.component.html'
+  templateUrl: './app/ts/app.component.html'
 })
 export class AppComponent { }
