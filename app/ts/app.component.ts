d42e585c8ed817e602066d7be59c70480c62ee8c
Vladimir Pavković <vladimirpavk@gmail.com>
2016-12-06 14:12:46 +0100

    Removed WebWorker

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,15 +1,8 @@
 /// <reference path="../../node_modules/@angular/common/index.d.ts" />
 import { Component, OnInit } from '@angular/core';
-import { WebWorker } from './worker';
 @Component({
   selector: 'my-app',
   templateUrl: './app/ts/app.component.html'
 })
-export class AppComponent implements OnInit{ 
-  
-  public worker:WebWorker;
-
-  ngOnInit(){
-      this.worker=new WebWorker();
-  }
+export class AppComponent{
 }
